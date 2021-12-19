323a6c67aab1b3cc93cb095c9862fa9656bd18fd
RiTerka <92330845+RiTerka@users.noreply.github.com>
2021-12-19 18:25:05 +0300

    check2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-const profileEdit = document.querySelector('.profile__edit');
+const profileEditBtn = document.querySelector('.profile__edit');
 const addBtn = document.querySelector('.profile__add');
 const closeBtn = document.querySelectorAll('.modal__close-btn');
 const editModal = document.querySelector('.modal-profile');
@@ -6,38 +6,15 @@ const addModal = document.querySelector('.modal-add');
 const imgModal = document.querySelector('.modal-img');
 const inputName = document.querySelector('.form__input_name_edit');
 const inputJob = document.querySelector('.form__input_info_edit');
+const imgTitle = document.querySelector('.form__input_title_edit');
+const imgLink = document.querySelector('.form__input_img_link');
 const profileTitle = document.querySelector('.profile__text-name');
 const profileSubtitle = document.querySelector('.profile__text-info');
 const imgList = document.querySelector('.element__items');
-const cards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+
 
 cards.forEach(elem => {
-    imgList.append(imgAdd(elem));
+    imgList.append(createCard(elem));
 });
 
 
@@ -59,24 +36,22 @@ function openAddCardForm() {
     openModal(addModal);
 }
 
-function editForm(evt) {
+function handleProfileFormSubmit(evt) {
     evt.preventDefault();
     profileTitle.textContent = inputName.value;
     profileSubtitle.textContent = inputJob.value;
     closeModal(editModal);
 }
 
-function addForm(evt) {
+function handleCardFormSubmit(evt) {
     evt.preventDefault();
-    const imgTitle = document.querySelector('.form__input_title_edit');
-    const imgLink = document.querySelector('.form__input_img_link');
-    const imgAddNew = imgAdd({ link: imgLink.value, name: imgTitle.value });
+    const imgAddNew = createCard({ link: imgLink.value, name: imgTitle.value });
     imgList.prepend(imgAddNew);
     document.forms['add-img'].reset();
     closeModal(addModal);
 }
 
-function imgAdd(elem) {
+function createCard(elem) {
     const imgElemTemplate = document.querySelector('#image-element-template').content;
     const imageCard = imgElemTemplate.cloneNode(true);
     const imgElemLink = imageCard.querySelector('.image-element__pic');
@@ -92,19 +67,19 @@ function imgAdd(elem) {
     imgElemDelete.addEventListener('click', (evt) => {
         evt.target.closest('.image-element').remove();
     });
-    function openimgModal() {
-        const figurePicture = document.querySelector('.modal-img');
+    function openImgModal() {
+        const figurePicture = document.querySelector('.modal__img');
         const figureTitle = document.querySelector('.modal__img-title');
         figurePicture.src = imgElemLink.src;
         figurePicture.alt = imgElemTitle.textContent;
         figureTitle.textContent = imgElemTitle.textContent;
         openModal(imgModal);
     }
-    imgElemLink.addEventListener("click", openimgModal);
+    imgElemLink.addEventListener("click", openImgModal);
     return imageCard;
 }
 
-profileEdit.addEventListener('click', openEditForm);
+profileEditBtn.addEventListener('click', openEditForm);
 addBtn.addEventListener('click', openAddCardForm);
 
 closeBtn.forEach((elem) => {
@@ -112,5 +87,5 @@ closeBtn.forEach((elem) => {
     elem.addEventListener('click', () => closeModal(activeModal));
 });
 
-editModal.addEventListener('submit', editForm);
-addModal.addEventListener('submit', addForm);
+editModal.addEventListener('submit', handleProfileFormSubmit);
+addModal.addEventListener('submit', handleCardFormSubmit);
